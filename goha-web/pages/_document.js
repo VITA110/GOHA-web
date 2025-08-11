20a49c9eff74f2c04494fd369f988b3aeeca2c84
JesusAMG <jesusalexism53@gmail.com>
2025-08-10 20:40:17 -0600

    si

--- goha-web/pages/_document.js
+++ goha-web/pages/_document.js
@@ -1,14 +1,15 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        {/* Aquí van cosas globales como meta tags, fuentes, etc. */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
