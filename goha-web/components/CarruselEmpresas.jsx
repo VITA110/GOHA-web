3cf3eba7fedc9322f329e93ec96eda25bfea9aea
VITA110 <hugo.cordova709@gmail.com>
2025-12-07 19:11:58 -0600

    Logos agregados correctamente

--- goha-web/components/CarruselEmpresas.jsx
+++ goha-web/components/CarruselEmpresas.jsx
@@ -6,17 +6,24 @@ const empresas = [
   { logo: 'logos/Microsoft.svg', nombre: 'Microsoft', pdf: '/catalogos/microsoft-catalogo.pdf' },
   { logo: 'logos/HP.svg', nombre: 'HP', pdf: '/catalogos/hp-catalogo.pdf' },
   { logo: 'logos/Adobe.svg', nombre: 'Adobe', pdf: '/catalogos/adobe-catalogo.pdf' },
+  { logo: '/logos/actualizados/Dell-logo.png', nombre: 'Dell', pdf: '' },
+  { logo: '/logos/actualizados/elo.png', nombre: 'ELO TOUCH', pdf: '' },
+  { logo: '/logos/actualizados/Epcom-log.png', nombre: 'Epcom', pdf: '' },
+  { logo: '/logos/actualizados/Honeywell-logo.png', nombre: 'Honeywell', pdf: '' },
   { logo: 'logos/Zebra.svg', nombre: 'Zebra', pdf: '/catalogos/zebra-catalogo.pdf' },
   { logo: 'logos/Intel.svg', nombre: 'Intel', pdf: '/catalogos/intel-catalogo.pdf' },
   { logo: 'logos/Cisco.svg', nombre: 'Cisco', pdf: '/catalogos/cisco-catalogo.pdf' },
   { logo: 'logos/Autodesk_Logo.svg', nombre: 'Asus', pdf: '/catalogos/cisco-catalogo.pdf' },
   { logo: '/logos/actualizados/APC-logo.png', nombre: 'APC', pdf: '' },
-  // { logo: 'logos/actualizados/Autodesk-Logo.svg', nombre: 'Autodesk', pdf: '' },
-  // { logo: 'logos/actualizados/AXIS-LOGO.png', nombre: 'Axis', pdf: '' },
-  // { logo: 'logos/actualizados/Brother-logo.png', nombre: 'Brother', pdf: '' },
-  // { logo: 'logos/actualizados/datalogic-logo.png', nombre: 'datalogic', pdf: '' },
+  { logo: '/logos/actualizados/AXIS-LOGO.png', nombre: 'Axis', pdf: '' },
+  { logo: '/logos/actualizados/Brother_logo.png', nombre: 'Brother', pdf: '' },
+  { logo: '/logos/actualizados/KAspersky_Lab_logo.png', nombre: 'Kaspersky', pdf: '' },
+  { logo: '/logos/actualizados/Lenovo_Logo.png', nombre: 'Lenovo', pdf: '' },
+  { logo: '/logos/actualizados/Lexmark-logo.png', nombre: 'Lexmark', pdf: '' },
+  { logo: '/logos/actualizados/Logitech_logo.png', nombre: 'Logitech', pdf: '' },
+  { logo: '/logos/actualizados/hikvision-logo.png', nombre: 'HIK VISION', pdf: '' },
+  
 ];
-
 export default function CarruselEmpresas() {
   const descargarPDF = (pdfUrl, nombreEmpresa) => {
     // Crear un elemento <a> temporal
