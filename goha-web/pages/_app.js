b400a74060026a0616faf26b61f352d272365d84
TizocPorras <sergioporras2003@gmail.com>
2026-05-21 16:27:55 -0600

    Cambio de descripciones

--- goha-web/pages/_app.js
+++ goha-web/pages/_app.js
@@ -1,5 +1,20 @@
-import "@/styles/globals.css";
+import Head from 'next/head'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>GOHA - International Operating Solutions</title>
+        <meta
+          name="description"
+          content="Impulsamos operaciones con tecnologías adaptadas a las exigencias de tu industria. Trabajamos con marcas reconocidas."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
